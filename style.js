8cfea052d30b183233c42ac72e3aa51427377f05
webd2276 <160004465+webd2276@users.noreply.github.com>
2024-09-10 15:00:35 +0500

    Add files via upload

---
+++ style.js
@@ -0,0 +1,13 @@
+let a=20;
+let b=30;
+let c=40;
+let d=40;
+let e=50;
+let f=60;
+     
+document.write((a===c)&&(el=f)+"");
+document.write((c===f)||(d<b)+"");
+document.write((a===c)&&(el!=f)+"");
+document.write((a===d)&&(el!=f)+"");
+document.write((e==f)&&(el>=d)+"");
+document.write((a=b)&&(e>=f)+"");
